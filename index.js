f9bd1fb3ec6bbbfbc551bbdc06de447b2c5bd5bd
pedroarissa <pedro.arissa@gmail.com>
2021-11-30 23:38:40 -0300

    -

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import chalk from 'chalk';
+
 const paragrafo = "Texto retornado por uma função";
 
 function texto(string) {
@@ -5,3 +7,21 @@ function texto(string) {
 }
 
 console.log(texto(paragrafo));
+
+console.log(chalk.blue(paragrafo))
+
+//encadear métodos para colorir texto, cor de fundo e texto em negrito
+console.log(chalk.blue.bgWhite.bold('Alura'));
+
+//receber múltiplos argumentos
+console.log(chalk.blue('curso', 'de', 'NodeJS'));
+
+//métodos aninhados
+console.log(chalk.red('vermelho', chalk.underline.bgBlue('azul')));
+
+// uso de template strings e placeholders
+console.log(`
+CPU: ${chalk.red('90%')}
+RAM: ${chalk.green('40%')}
+DISK: ${chalk.yellow('70%')}
+`);
